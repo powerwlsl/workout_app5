671dc6420059bcd7caf525227a838978f879f1e7
Hyejin Ahn <thebestisyet2com@gmail.com>
2016-12-18 14:39:32 -0800

    Add numericality validation for duration attribute

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,7 @@
 class Exercise < ApplicationRecord
   belongs_to :user
-  validates :duration, presence: true
+  validates :duration, presence: true, numericality: {greater_than: 0.0}
   validates :detail, presence: true
   validates :date, presence: true
-
+  # alias_attribute :  => if you want to use different name from db
 end
