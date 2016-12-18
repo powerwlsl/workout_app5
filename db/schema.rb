fc164cab4111c365b5dd04c58f65ace4571d6896
Hyejin Ahn <thebestisyet2com@gmail.com>
2016-12-18 13:07:38 -0800

    Add routes, index and new actions of exercise controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218094611) do
+ActiveRecord::Schema.define(version: 20161218200702) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.integer  "duration"
+    t.text     "detail"
+    t.date     "date"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_exercises_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
