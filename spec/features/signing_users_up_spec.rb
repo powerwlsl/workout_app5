b30a8ca161e4be7da2576384a51b3031f720633e
Hyejin Ahn <thebestisyet2com@gmail.com>
2016-12-18 23:03:32 -0800

    Change signing user up spec, add first name and last name

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -4,10 +4,14 @@ RSpec.feature "User signup" do
   scenario "with valid credential" do 
     visit "/"
     click_link "Sign up"
+    fill_in "First name", with: "Hyejin"
+    fill_in "Last name", with: "Ahn"
     fill_in "Email", with: "example@gmail.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     expect(page).to have_content("You have signed up successfully")
+    expect(page).to have_content("Hyejin Ahn")
+
   end
 end
