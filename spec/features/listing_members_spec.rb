44ff1cf762daed576acec4e68fae8d42994f0013
Hyejin Ahn <thebestisyet2com@gmail.com>
2016-12-18 22:59:54 -0800

    List members on homepage

--- spec/features/listing_members_spec.rb
+++ spec/features/listing_members_spec.rb
@@ -6,4 +6,12 @@ RSpec.feature "Listing members" do
     @user2 = User.create!(first_name:"Jane",last_name:"Doe",email:"example2@gmail.com", password:"password")
     login_as(@user1)
   end
+
+  scenario "shows a list of registered members" do 
+    visit '/'
+    expect(page).to have_content("List of Members")
+    expect(page).to have_content(@user1.full_name)
+    expect(page).to have_content(@user2.full_name)
+
+  end
 end
